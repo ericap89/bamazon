2196c1a88abb823a9eaeceabbed20d369328bad1
Erica P <erica.parra@rocketmail.com>
2019-05-06 21:45:17 -0700

    query for specific data succesfully added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,11 +10,22 @@ db = createConnection({
 })
 
 // db.connect(e => e ? console.log(e) : console.log('Connected...'))
+
 db.connect(e => {
     if (e) { console.log(e) }
     //Display Products
     db.query('SELECT * FROM products', (e, data) => {
-        if (e) { console.log(e) }
-        console.log(data)
+        if (e) { console.log(e) } else {
+            data.forEach(({ item_id, product_name, price}) => {
+                console.log(`
+                **************
+                Product: ${product_name}
+                Price: ${price}
+                Product ID: ${item_id}
+                **************
+                `)
+            })
+        }
+        // console.log(data)
     })
 })
