7077e9471a78186a39cb454dd426e0bd616aecfc
Erica P <erica.parra@rocketmail.com>
2019-05-06 21:38:16 -0700

    all data query added succesfully

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,4 +9,12 @@ db = createConnection({
     database:'bamazon'
 })
 
-db.connect(e => e ? console.log(e) : console.log('Connected...'))
+// db.connect(e => e ? console.log(e) : console.log('Connected...'))
+db.connect(e => {
+    if (e) { console.log(e) }
+    //Display Products
+    db.query('SELECT * FROM products', (e, data) => {
+        if (e) { console.log(e) }
+        console.log(data)
+    })
+})
